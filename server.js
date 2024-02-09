7356158883b8e9ad421e4255f5d7ff52fa74007d
Prabin <prabin.prakash@publicissapient.com>
2024-02-09 18:09:05 +0400

    dot env added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,12 +14,11 @@ app.use(express.json());
 app.post('/api/sendMessage', async (req, res) => {
   try {
     const message = JSON.stringify(req.body);
-
+    console.log('env', process.env.AZURE_AI_API_KEY);
 
     // Replace 'YOUR_AZURE_API_ENDPOINT' and 'YOUR_AZURE_API_KEY' with your actual values
     const azureApiEndpoint = process.env.AZURE_END_POINT_URL;
     const azureApiKey = process.env.AZURE_AI_API_KEY;
-
     const response = await axios.post(azureApiEndpoint,  message , {
       headers: {
         'Content-Type': 'application/json',
